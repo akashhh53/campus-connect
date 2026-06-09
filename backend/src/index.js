6326f1638aa1b11737efdc6673c2ac149d833c2d
Akash Singh <akashsingh.work53@gmail.com>
2026-06-09 20:24:46 +0530

    fix cors deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-require('dotenv').config();
-const main = require('./config/db');
-const cookierParser = require('cookie-parser');
-const router = require('./routes/userauth');
-const redisClient = require('./config/redis');
-const lostFoundRoutes = require('./routes/lostFoundRoutes'); 
-const feedRoutes = require('./routes/feedRoutes');
+
+require("dotenv").config();
+
+const main = require("./config/db");
+const cookierParser = require("cookie-parser");
+const router = require("./routes/userauth");
+const redisClient = require("./config/redis");
+const lostFoundRoutes = require("./routes/lostFoundRoutes");
+const feedRoutes = require("./routes/feedRoutes");
+
 const cors = require("cors");
-
-
-
 
 app.use(
   cors({
@@ -19,29 +19,27 @@ app.use(
   })
 );
 
-app.use(express.json());
-app.options("*", cors());
 // Middleware
 app.use(express.json());
 
 app.use(cookierParser());
 
+app.use("/user", router);
+app.use("/user", lostFoundRoutes);
+app.use("/user", feedRoutes);
 
-app.use('/user',router);
-app.use('/user', lostFoundRoutes); 
-app.use('/user', feedRoutes);   
+const InitializeConnection = async () => {
+  try {
+    await Promise.all([main(), redisClient.connect()]);
 
-const InitializeConnection=async()=>{
-    try{
-       await Promise.all([main(),redisClient.connect()]);
-       console.log('Connected to MongoDB and Redis');
-         app.listen(process.env.PORT, () => {
-         console.log(`Server is running on port ${process.env.PORT}`);
-          });
-    }
-    catch(err){
-        console.log(err);
-    }
-}
+    console.log("Connected to MongoDB and Redis");
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 InitializeConnection();
-
