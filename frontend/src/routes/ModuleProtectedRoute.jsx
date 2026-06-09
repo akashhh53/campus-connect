afaf65f5017d6cdcc62a91a1a6cb2fb63c336e50
Akash Singh <akashsingh.work53@gmail.com>
2026-06-09 21:17:25 +0530

    fix module auth persistence

--- frontend/src/routes/ModuleProtectedRoute.jsx
+++ frontend/src/routes/ModuleProtectedRoute.jsx
@@ -1,15 +1,24 @@
 import { Navigate } from "react-router";
-
 import { useSelector } from "react-redux";
 
 const ModuleProtectedRoute = ({
   children,
   moduleName,
 }) => {
+  const authState =
+    useSelector((state) => state.auth);
 
-  const { user } = useSelector(
-    (state) => state.auth
-  );
+  let user = authState.user;
+
+  if (!user) {
+    const stored =
+      localStorage.getItem("userInfo");
+
+    if (stored) {
+      user =
+        JSON.parse(stored)?.user;
+    }
+  }
 
   const allowedModules =
     user?.role?.allowedModules || {};
