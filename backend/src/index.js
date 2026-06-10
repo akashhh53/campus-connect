ab91eebef5529135ffc256df73b41565de055da2
Akash Singh <akashsingh.work53@gmail.com>
2026-06-10 17:16:00 +0530

    test refresh token flow

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [process.env.FRONTEND_URL,"http://localhost:5173"],
     credentials: true,
   })
 );
--- backend/src/utils/token.js
+++ backend/src/utils/token.js
@@ -8,7 +8,7 @@ const generateAccessToken = (user) => {
       role: user.role
     },
     process.env.JWT_KEY,
-    { expiresIn: '30m' }
+    { expiresIn: '30s' }
   );
 };
 
